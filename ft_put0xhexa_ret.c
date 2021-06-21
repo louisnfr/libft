6ca676c9529f5f8f0a5f3bc54f3d3898040b91fa
Louis RAFFIN <lraffin@e3r4p8.42.fr>
2021-06-21 17:00:03 +0200

    hop

--- ft_put0xhexa_ret.c
+++ ft_put0xhexa_ret.c
@@ -6,25 +6,19 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 14:05:41 by lraffin           #+#    #+#             */
-/*   Updated: 2021/06/15 16:23:31 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/06/21 16:48:07 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static void	ft_print0xhexa(unsigned long n, const char *base, int *count)
-{
-	if (n >= 16)
-		ft_print0xhexa(n / 16, base, count);
-	*count += write(1, &base[n % 16], 1);
-}
 
 int	ft_put0xhexa_ret(unsigned long n, const char *base)
 {
 	int	count;
 
 	count = 0;
-	count += ft_putstr_ret("0x");
-	ft_print0xhexa(n, base, &count);
+	if (n >= 16)
+		ft_put0xhexa_ret(n / 16, base);
+	count += write(1, &base[n % 16], 1);
 	return (count);
 }
