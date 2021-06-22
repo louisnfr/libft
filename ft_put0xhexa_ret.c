ffd558c0f43782891c373d6334b4b84a2040a559
Louis RAFFIN <lraffin@e3r4p8.42.fr>
2021-06-22 15:08:24 +0200

    fix puthexa

--- ft_put0xhexa_ret.c
+++ ft_put0xhexa_ret.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 14:05:41 by lraffin           #+#    #+#             */
-/*   Updated: 2021/06/21 16:48:07 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/06/22 12:13:13 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_put0xhexa_ret(unsigned long n, const char *base)
 
 	count = 0;
 	if (n >= 16)
-		ft_put0xhexa_ret(n / 16, base);
+		count += ft_put0xhexa_ret(n / 16, base);
 	count += write(1, &base[n % 16], 1);
 	return (count);
 }
