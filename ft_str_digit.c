6739f0a267ad48ad92e57fa4b128531786c80b64
louisnfr <louis.raffin@gmail.com>
2021-09-17 18:43:53 +0200

    libft

--- ft_str_digit.c
+++ ft_str_digit.c
@@ -6,11 +6,16 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 17:47:42 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/17 17:52:12 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/17 18:42:25 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	is_space(int c)
+{
+	return (c == 32);
+}
 
 int	ft_str_digit(char *s)
 {
@@ -18,7 +23,7 @@ int	ft_str_digit(char *s)
 
 	i = -1;
 	while (s && s[++i])
-		if (!ft_isdigit(s[i]))
+		if (!ft_isdigit(s[i]) || !is_space(s[i]))
 			return (0);
 	return (1);
 }
